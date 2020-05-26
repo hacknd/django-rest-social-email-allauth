28c4a15669c91160338bb46b53a2926ddf0c415d
Benard18 <benkaranja43@gmail.com>
2020-05-26 14:56:20 +0300

    Bug Fixed

--- rest_social_email_auth/models.py
+++ rest_social_email_auth/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
-from django.utils.translation import getext_lazy as __
+from django.utils.translation import gettext_lazy as __
 
 #Local Modules
 from rest_social_email_auth import managers
