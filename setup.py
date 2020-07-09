3049cf767b90057f39bd18066947c06620e01483
Benard18 <benkaranja43@gmail.com>
2020-07-09 22:21:34 +0300

    setup file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 	long_description = f.read()
 
 setup(
@@ -16,9 +16,10 @@ setup(
 	version='0.1.0',
 
 	# Description Area
-	description="This is an authentication module template with social (Oauth2) Authentication, knox-rest and email account verification with password reset framework.With bot regulations(Under construction).",
+	description="This is an authentication module template with social (Oauth2) Authentication, knox-rest and email account verification with password reset framework.",
 	long_description=long_description,
-	long_description_content_type="text/markdown",
+	long_description_content_type="text/x-rst",
+	license="MIT",
 	url="https://github.com/hacknd/django-rest-social-email-auth",
 	author="HackND",
 	author_email="benkaranja43@gmail.com",
@@ -28,7 +29,6 @@ setup(
 		# Supported versions of Django
         "Framework :: Django",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         # Supported versions of Python
         "Programming Language :: Python",
