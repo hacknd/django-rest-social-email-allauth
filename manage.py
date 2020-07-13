081abd66d84fc8087abfc8f46b9d69ceadb360d5
Benard18 <benkaranja43@gmail.com>
2020-07-13 18:32:05 +0300

    manage update

--- manage.py
+++ manage.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     os.environ["DJANGO_SETTINGS_MODULE"] = "tests.settings"
     from django.core.management import execute_from_command_line
 
-    execute_from_command_line(sys.argv)
+    execute_from_command_line()
